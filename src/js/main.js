25649651a864eff205bde1fd35c7176d819b3da6
Andrey Hrusheuski <gruand13@gmail.com>
2022-08-16 18:44:17 +0300

    add timer  on site

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,12 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import  changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
 
 window.addEventListener('DOMContentLoaded', ()=>{
     "use strict";
     let modalState = {};
+    let deadline = '2022-10-05';
 
     changeModalState(modalState);
     modals();
@@ -15,4 +17,5 @@ window.addEventListener('DOMContentLoaded', ()=>{
     tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
 
     forms(modalState);
+    timer('.container1', deadline);
 });
